9c53a38d05ec587479e0e1d61779555d112df5fb
kikito <nikita.tyunyayev@student.uclouvain.be>
2022-08-01 13:04:22 +0000

    using msquic buildin testing tool

---
+++ client.sh
@@ -0,0 +1,2 @@
+
+sudo ./artifacts/bin/linux/x64_Release_openssl/secnetperf -TestName:throughput -target:10.100.0.2 -p 4443 -download:20000000000
---
+++ server.sh
@@ -0,0 +1,1 @@
+sudo ./artifacts/bin/linux/x64_Release_openssl/secnetperf -bind:10.100.0.2
