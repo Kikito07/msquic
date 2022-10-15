0b6659118b4b97b60f0c431e616ec9a31254013c
kikito <nikita.tyunyayev@student.uclouvain.be>
2022-10-15 11:31:32 +0000

    changin client server scripts

--- client.sh
+++ client.sh
@@ -1,2 +1,2 @@
-
-sudo ./artifacts/bin/linux/x64_Release_openssl/secnetperf -TestName:throughput -target:10.100.0.2 -p 4443 -download:20000000000
+#sudo perf record --call-graph dwarf -o /home/nikita/memoire/dpdk_picoquic/perfMeasures/perf_msquic_client.data taskset -c 4 ./artifacts/bin/linux/x64_Release_openssl/secnetperf -cipher:1 -TestName:throughput -target:10.100.0.2 -p 4443 -download:20000000000
+sudo chrt -r 1 taskset -c 4,5,6 ./artifacts/bin/linux/x64_Release_openssl/secnetperf -cipher:1 -TestName:throughput -target:10.100.0.2 -p 4443 -download:20000000000
--- server.sh
+++ server.sh
@@ -1,1 +1,2 @@
-sudo ./artifacts/bin/linux/x64_Release_openssl/secnetperf -bind:10.100.0.2
+#sudo perf record --call-graph dwarf -o /home/nikita/memoire/dpdk_picoquic/perfMeasures/perf_msquic_server.data sudo taskset -c 4 ./artifacts/bin/linux/x64_Release_openssl/secnetperf -cipher:1 -bind:10.100.0.2
+sudo chrt -r 1 taskset -c 4,5,6 ./artifacts/bin/linux/x64_Release_openssl/secnetperf -cipher:1 -bind:10.100.0.2
